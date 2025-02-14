87535ef60e03cae54636735ed7f5ced9f04a05c3
“Rohan <2914897r@student.gla.ac.uk>
2025-02-14 10:41:27 +0000

    Commit chapter 7 tests ok

--- rango/models.py
+++ rango/models.py
@@ -4,6 +4,8 @@ from django.template.defaultfilters import slugify
 # Create your models here.
 
 class Category(models.Model):
+    MAX_LENGTH = 128
+
     name = models.CharField(max_length=128, unique=True)
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
@@ -20,8 +22,10 @@ class Category(models.Model):
         return self.name
 
 class Page(models.Model):
+    MAX_LENGTH = 128
+
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    title = models.CharField(max_length=128)
+    title = models.CharField(max_length=MAX_LENGTH)
     url = models.URLField()
     views = models.IntegerField(default=0)
 
