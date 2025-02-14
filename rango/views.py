f4eac25ef9cd2eb3c018e4288628b75e748c51ec
“Rohan <2914897r@student.gla.ac.uk>
2025-02-14 10:01:35 +0000

    Chapter 6 OK forgot chapter 5

--- rango/views.py
+++ rango/views.py
@@ -1,14 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from rango.models import Category
+from rango.models import Page
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
+    page_list = Page.objects.order_by('-views')[:5]
 
     context_dict = {}
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
+    context_dict['pages'] = page_list
 
     return render(request, 'rango/index.html', context=context_dict)
+    
 def about(request):
-    return render(request, 'rango/about.html') 
+    return render(request, 'rango/about.html')
+
+def show_category(request, category_name_slug):
+    context_dic = {}
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        pages = Page.objects.filter(category=category)
+        context_dic['pages'] = pages
+        context_dic['category'] = category
+    except Category.DoesNotExist:
+        context_dic['category'] = None
+        context_dic['pages'] = None
+    return render(request, 'rango/category.html', context=context_dic)
